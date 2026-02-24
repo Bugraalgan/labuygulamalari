f361d93279bf4cd38d811f4db0ee2ec2d9ca1138
Buğra <bugraallgan@gmail.com>
2026-02-24 13:06:42 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+ function App() {
+
+return (
+<div>
+<h1>Web Tasarimi ve Programlama</h1>
+<h2>LAB-1</h2>
+<p>Ad Soyad: [Buğra ALGAN ]</p>
+<p>Ogrenci No: [230541094]</p>
+<p>Bolum : [Yazilim muhendisligi]</p>
+<p>Sinif : [3]</p>
+<p>Not ortalamasi:[2.70]</p>
+</div>
+)
+ }
+export default App
